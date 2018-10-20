6d55a6b3079a8f58e7864c044ea590a479103fd7
remmi11 <wtgeographer@gmail.com>
2018-10-20 14:30:55 -0500

    init comit

--- conn.php
+++ conn.php
@@ -1,5 +1,5 @@
 <?php
     // Connecting, selecting database
-    $conn_string = pg_connect("host=nutty-blueberry.db.elephantsql.com dbname=time user=jcfcqrso password=ExNoFtbZT7_BNnmb5XFIwDIDwJHOVDK0")
+    $conn_string = pg_connect("host=13.82.41.95 dbname=time user=postgres password=gdipass1")
         or die('Could not connect: ' . pg_last_error());
 ?>
